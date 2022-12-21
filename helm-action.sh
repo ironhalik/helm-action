e1ef99a5ebaf03729529158dd56d334a00d8ba5f
Michał Weinert <michal@weinert.io>
2022-12-21 15:16:30 +0100

    Added env var support; moved NAMESPACE to parent action; shellcheck fixes; shellcheck CI

--- helm-action.sh
+++ helm-action.sh
@@ -1,39 +1,56 @@
+# shellcheck shell=bash
 # This code is meant to be used with
 # https://github.com/ironhalik/kubectl-action-base
-if [ ! -n "${IS_KUBECTL_ACTION_BASE}" ]; then
+if [ -z "${IS_KUBECTL_ACTION_BASE}" ]; then
     echo "::error:: The script is not meant to be used on it's own."
     exit 1
 fi
 
 # helm-action specific code
-HELM_ARGS="--namespace ${INPUT_NAMESPACE}"
-[ "${INPUT_CREATE_NAMESPACE}" == "true" ] && HELM_ARGS="${HELM_ARGS} --create-namespace"
-[ "${INPUT_ATOMIC}" == "true" ] && HELM_ARGS="${HELM_ARGS} --atomic"
-[ "${INPUT_WAIT}" == "true" ] && HELM_ARGS="${HELM_ARGS} --wait"
-[ -n "${INPUT_TIMEOUT}" ] && HELM_ARGS="${HELM_ARGS} --timeout ${INPUT_TIMEOUT}"
-[ -n "${INPUT_VALUES_FILES}" ] && for file in "${INPUT_VALUES_FILES//,/}"; do HELM_ARGS="${HELM_ARGS} --values ${file}"; done
-[ -n "${INPUT_VALUES}" ] && echo "${INPUT_VALUES}" > /tmp/gha_input_values.yaml && HELM_ARGS="${HELM_ARGS} --values /tmp/gha_input_values.yaml"
+# input prep
+CREATE_NAMESPACE="${INPUT_CREATE_NAMESPACE:-${CREATE_NAMESPACE}}"
+RELEASE="${INPUT_RELEASE:-${RELEASE}}"
+CHART="${INPUT_CHART:-${CHART}}"
+APP_VERSION="${INPUT_APP_VERSION:-${APP_VERSION}}"
+VALUES="${INPUT_VALUES:-${VALUES}}"
+VALUES_FILES="${INPUT_VALUES_FILES:-${VALUES_FILES}}"
+ATOMIC="${INPUT_ATOMIC:-${ATOMIC}}"
+WAIT="${INPUT_WAIT:-${WAIT}}"
+TIMEOUT="${INPUT_TIMEOUT:-${TIMEOUT}}"
+GITHUB_SUMMARY="${INPUT_GITHUB_SUMMARY:-${GITHUB_SUMMARY}}"
+GITHUB_SUMMARY_STRIP_COMMANDS="${INPUT_GITHUB_SUMMARY_STRIP_COMMANDS:-${GITHUB_SUMMARY_STRIP_COMMANDS}}"
+
+
+HELM_ARGS="--namespace ${NAMESPACE}"
+[ "${CREATE_NAMESPACE}" == "true" ] && HELM_ARGS="${HELM_ARGS} --create-namespace"
+[ "${ATOMIC}" == "true" ] && HELM_ARGS="${HELM_ARGS} --atomic"
+[ "${WAIT}" == "true" ] && HELM_ARGS="${HELM_ARGS} --wait"
+[ -n "${TIMEOUT}" ] && HELM_ARGS="${HELM_ARGS} --timeout ${TIMEOUT}"
+[ -n "${VALUES_FILES}" ] && for file in ${VALUES_FILES//,/}; do HELM_ARGS="${HELM_ARGS} --values ${file}"; done
+[ -n "${VALUES}" ] && echo "${VALUES}" > /tmp/gha_input_values.yaml && HELM_ARGS="${HELM_ARGS} --values /tmp/gha_input_values.yaml"
 [ -n "${IS_DEBUG}" ] && HELM_ARGS="${HELM_ARGS} --debug"
-HELM_ARGS="${HELM_ARGS} ${INPUT_CHART}"
+HELM_ARGS="${HELM_ARGS} ${CHART}"
 
-[ -n "${INPUT_APP_VERSION}" ] && yq -i ".appVersion = \"${INPUT_APP_VERSION}\"" ${INPUT_CHART}/Chart.yaml
+[ -n "${APP_VERSION}" ] && yq -i ".appVersion = \"${APP_VERSION}\"" "${CHART}/Chart.yaml"
 
-[ -n "${INPUT_VALUES}" ] && log debug "Values:\n$(cat /tmp/gha_input_values.yaml)"
-log debug "Running: helm template ${INPUT_RELEASE} ${HELM_ARGS})\n"
-log debug "$(helm template ${INPUT_RELEASE} ${HELM_ARGS})"
+[ -n "${VALUES}" ] && log debug "Values:\n$(cat /tmp/gha_input_values.yaml)"
+log debug "Running: helm template ${RELEASE} ${HELM_ARGS}\n"
+# shellcheck disable=SC2086
+log debug "$(helm template ${RELEASE} ${HELM_ARGS})"
 
-log info "Running: helm upgrade ${INPUT_RELEASE} --install ${HELM_ARGS}"
+log info "Running: helm upgrade ${RELEASE} --install ${HELM_ARGS}"
 set +e
-helm upgrade ${INPUT_RELEASE} --install ${HELM_ARGS}
+# shellcheck disable=SC2086
+helm upgrade "${RELEASE}" --install ${HELM_ARGS}
 helm_status="${?}"
 log debug "Helm exited with status ${helm_status}"
 
-if [ "${INPUT_GITHUB_SUMMARY}" == "true" ]; then
+if [ "${GITHUB_SUMMARY}" == "true" ]; then
     log debug "Writing helm status to ${GITHUB_STEP_SUMMARY}"
-    if [ "${INPUT_GITHUB_SUMMARY_STRIP_COMMANDS}" == "true" ]; then
-        helm status --namespace "${INPUT_NAMESPACE}" "${INPUT_RELEASE}" | grep -Ev '^::.+::.+' >> "${GITHUB_STEP_SUMMARY}"
+    if [ "${GITHUB_SUMMARY_STRIP_COMMANDS}" == "true" ]; then
+        helm status --namespace "${NAMESPACE}" "${RELEASE}" | grep -Ev '^::.+::.+' >> "${GITHUB_STEP_SUMMARY}"
     else
-        helm status --namespace "${INPUT_NAMESPACE}" "${INPUT_RELEASE}" >> "${GITHUB_STEP_SUMMARY}"
+        helm status --namespace "${NAMESPACE}" "${RELEASE}" >> "${GITHUB_STEP_SUMMARY}"
     fi
 fi
 
